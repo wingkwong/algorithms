f87e60aa8a606a03efe2ce7a04663071e104e8a4
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-07-05 22:18:25 +0800

    Huffman Coding

--- huffman-coding.cpp
+++ huffman-coding.cpp
@@ -1,3 +1,7 @@
+// Huffman Coding is a lossless data compression algorithm 
+// that works by assigning codes to symbols in a way 
+// that minimizes the total number of bits required to represent a message.
+
 #include <bits/stdc++.h>
 using namespace std;
 
--- huffman-coding/README.md
+++
@@ -1,3 +0,0 @@
-## Huffman Coding
-
-Huffman Coding is a lossless data compression algorithm that works by assigning codes to symbols in a way that minimizes the total number of bits required to represent a message.
